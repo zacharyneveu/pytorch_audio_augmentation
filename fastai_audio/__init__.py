b9f03213235f5857484719df7d0e45bbac30fdb0
Zach Neveu <Zachary_Neveu@bose.com>
2019-03-20 12:24:46 -0400

    remove fastai_audio fork

--- fastai_audio/__init__.py
+++
@@ -1,9 +0,0 @@
-from .audio_clip import *
-from .data import *
-from .learner import *
-from .metrics import *
-from .transform import *
-from .tta import *
-
-__all__ = [*audio_clip.__all__, *data.__all__, *learner.__all__,
-           *metrics.__all__, *transform.__all__, *tta.__all__]
